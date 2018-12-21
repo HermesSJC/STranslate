5d8d08e4e481ff206a73993846197dcf48e0a510
HermesSJC <41092685+HermesSJC@users.noreply.github.com>
2018-12-21 14:00:54 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,170 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    fileData.clear();
+    texteditData.clear();
+    isCreateFile = false;
+
+    dateTime = new QDateTime();
+
+    ui->fromComboBox->addItem("English","en");
+    ui->fromComboBox->addItem("Chinese","zh");
+    ui->fromComboBox->addItem("Auto","auto");
+    ui->fromComboBox->setCurrentIndex(0);
+
+    ui->toComboBox->addItem("English","en");
+    ui->toComboBox->addItem("Chinese","zh");
+    ui->toComboBox->setCurrentIndex(1);
+
+    networkManager = new QNetworkAccessManager(this);
+    this->connect(networkManager,SIGNAL(finished(QNetworkReply *)),this,SLOT(on_replyFinished(QNetworkReply *)));
+
+
+    if(!dirPath.exists("Translate"))
+    {
+        dirPath.mkdir("Translate");
+    }
+
+    translateFile = new QFile(dirPath.path().append("/Translate/history_%1.txt").arg(CurrentTime()));
+    if(translateFile->open(QIODevice::Text | QIODevice::Append))
+    {
+        translateTextStream = new QTextStream(translateFile);
+        isCreateFile = true;
+    }
+    else
+    {
+        ui->statusLabel->setText("Translate file create failed");
+    }
+}
+
+Widget::~Widget()
+{
+    translateFile->close();
+
+    delete ui;
+}
+
+void Widget::on_chooseFileButton_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this,QString("Open"),dirPath.path(),"* txt");
+    if(filePath.isEmpty())
+    {
+        ui->statusLabel->setText("Not choose any file!");
+        return ;
+    }
+
+    QFile file(filePath);
+    if(file.open(QIODevice::ReadOnly))
+    {
+        fileData.clear();
+        fileData.append(file.readAll());
+    }
+    else
+    {
+        ui->statusLabel->setText("Open file failed!");
+    }
+}
+
+void Widget::on_translateButton_clicked()
+{
+    srcData.clear();
+    texteditData.clear();
+    texteditData = ui->srcTextEdit->toPlainText();
+    if(!texteditData.isEmpty())
+    {
+        ui->translateTextEdit->clear();
+        ui->translateTextEdit->document()->clear();
+
+        texteditData.replace("\n", " ");
+
+        srcData.append(texteditData);
+        texteditData.clear();
+
+    }
+    else if(!fileData.isEmpty() && isCreateFile)
+    {
+        ui->translateTextEdit->clear();
+        ui->translateTextEdit->document()->clear();
+
+        fileData.replace("\r\n", " ");
+
+        srcData.append(fileData);
+
+        fileData.clear();
+    }
+    else
+    {
+        ui->statusLabel->setText("There is no translate data!");
+        return;
+    }
+
+    QString from = ui->fromComboBox->currentData().toString();
+    QString to = ui->toComboBox->currentData().toString();
+    QString md5 = GetSignMD5().toHex();
+
+    QString status = QString("q=%1&from=%2&to=%3&appid=%4&salt=%5&sign=%6").arg(srcData).arg(from).arg(to).arg(SSign.appID).arg(SSign.salt).arg(md5);
+
+    QNetworkRequest request;
+    request.setUrl(QUrl("http://api.fanyi.baidu.com/api/trans/vip/translate?"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
+    request.setHeader(QNetworkRequest::ContentLengthHeader,status.length());
+    networkManager->post(request,status.toUtf8());
+}
+
+void Widget::on_clearButton_clicked()
+{
+    ui->srcTextEdit->clear();
+    ui->srcTextEdit->document()->clear();
+
+    ui->translateTextEdit->clear();
+    ui->translateTextEdit->document()->clear();
+
+    fileData.clear();
+    texteditData.clear();
+}
+
+void Widget::on_historyButton_clicked()
+{
+    //保存文件 可以打开历史txt
+    translateFile->close();
+    translateFile->open(QIODevice::Text | QIODevice::Append);
+}
+
+void Widget::on_replyFinished(QNetworkReply *reply)
+{
+    QVariant rep = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+    if(reply->error() == QNetworkReply::NoError)
+    {
+        QByteArray data = reply->readAll();
+
+        QJsonObject jsData(QJsonDocument::fromJson(data).object());
+        QString string = jsData["trans_result"].toArray()[0].toObject()["dst"].toString();
+
+        ui->translateTextEdit->clear();
+        ui->translateTextEdit->document()->clear();
+        ui->translateTextEdit->insertPlainText(string);
+        *translateTextStream << string << "\r\n";
+
+        ui->statusLabel->setText("Request Success!");
+    }
+    else
+    {
+        ui->statusLabel->setText("Request Error!");
+    }
+}
+
+
+QByteArray Widget::GetSignMD5()
+{
+    QString sign;
+    SSign.q = srcData;
+    sign = SSign.appID + SSign.q + SSign.salt + SSign.appPassword;
+    QByteArray md5 = QCryptographicHash::hash(sign.toUtf8(), QCryptographicHash::Md5);
+    return md5;
+}
---
+++ widget.h
@@ -0,0 +1,80 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QString>
+#include <QDir>
+#include <QDebug>
+#include <QTextStream>
+#include <QTextEdit>
+#include <QLabel>
+#include <QFileDialog>
+#include <QDateTime>
+#include <QUrl>
+
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QCryptographicHash>
+#include <QJsonObject>
+
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_chooseFileButton_clicked();
+
+    void on_translateButton_clicked();
+
+    void on_clearButton_clicked();
+
+    void on_historyButton_clicked();
+
+private:
+
+    struct STranslateSign
+    {
+        QString appID = "";                 //app的id
+        QString q;                          //要翻译的字符
+        QString salt = "1466358802";        //随机数 这边可以随便换
+        QString appPassword = "";           //app的id密码
+    } SSign;
+
+    QByteArray GetSignMD5();
+
+
+    Ui::Widget *ui;
+
+    QString fileData;
+    QString texteditData;
+    QString srcData;
+
+    QDir dirPath;
+    QFile *translateFile;
+    QTextStream *translateTextStream;
+
+    bool isCreateFile;
+
+    QDateTime *dateTime;
+    inline QString CurrentTime(QString);
+
+    QNetworkAccessManager *networkManager;
+
+private slots:
+    void on_replyFinished(QNetworkReply *reply);
+
+};
+
+inline QString Widget::CurrentTime(QString font = "hhmm") { return dateTime->currentDateTime().toString(font); }
+
+#endif // WIDGET_H
